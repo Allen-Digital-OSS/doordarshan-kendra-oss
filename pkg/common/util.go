4bfc11b0f51e8a7160679dc3495b98f770f29e3f
chiragmahajan <chirag.mahajan@allen.in>
2026-02-17 16:02:49 +0530

    removed unused code

--- pkg/common/util.go
+++ pkg/common/util.go
@@ -1,15 +1,9 @@
 package common
 
 import (
-	"errors"
 	"github.com/Allen-Digital-OSS/doordarshan-kendra-oss/pkg/constant"
 	"os"
 	"strings"
-)
-
-var (
-	ErrUnauthorizedEmptyAuthHeader         = errors.New("auth header is empty")
-	ErrUnauthorizedIllegalAuthHeaderFormat = errors.New("auth header format is illegal: missing Bearer prefix")
 )
 
 // GetCurrentEnvironment returns the current environment.
